00fc1bc01d240946e64436894d7b75f30450c36a
Val Currie <contact@valcuri.email>
2021-05-19 15:08:43 -0500

    fixed missing .js in import

--- src/scripts/User/UserProvider.js
+++ src/scripts/User/UserProvider.js
@@ -1,4 +1,4 @@
-import {rawUsers} from "../data/provider";
+import {rawUsers} from "../data/provider.js";
 
 // we aren't using it yet but if we want to add logic
 //      to getting the Users data we can do it here
