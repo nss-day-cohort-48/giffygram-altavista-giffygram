2feb7c84098c9c10d24a39b5ba4b3e794b5c8556
Val Currie <contact@valcuri.email>
2021-05-19 15:02:48 -0500

    added fetch and raw get for users

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,22 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
-
+const apiURL = "http://localhost:8088";
+const applicationElement = document.querySelector(".giffygram");
 
 const applicationState = {
-    currentUser: {},
-    feed: {
-        chosenUser: null,
-        displayFavorites: false,
-        displayMessages: false
-    }
-}
+  currentUser: {},
+  feed: {
+    chosenUser: null,
+    displayFavorites: false,
+    displayMessages: false,
+  },
+  users: [],
+};
+
+export const rawUsers = () => applicationState.users.map((t) => ({...t}));
+
+const apiURL = "http://localhost:8081";
+
+export const fetchUsers = () => {
+  return fetch(`${apiURL}/products`)
+    .then((response) => response.json())
+    .then((data) => (applicationState.users = data));
+};
