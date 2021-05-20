4f8853a60cf5a2c036812c3fc6ce61d5c50bbca5
Val Currie <contact@valcuri.email>
2021-05-20 09:37:07 -0500

    wired up the statechanged

--- src/scripts/User/UserProvider.js
+++ src/scripts/User/UserProvider.js
@@ -2,7 +2,7 @@ import {rawLikes, rawPosts, rawUsers} from "../data/provider.js";
 
 // we aren't using it yet but if we want to add logic
 //      to getting the Users data we can do it here
-export const GetUsers = () => {
+export const getUsers = () => {
   let users = rawUsers();
 
   users = addLikes(users, rawLikes());
