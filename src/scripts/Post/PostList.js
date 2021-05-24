c5c8884090fea55a0082bf0d27f48b61a978a46c
Val Currie <contact@valcuri.email>
2021-05-24 15:45:45 -0500

    Created a like provider and added a like button to each post

--- src/scripts/Post/PostList.js
+++ src/scripts/Post/PostList.js
@@ -1,24 +1,52 @@
+import {toggleLike} from "../Like/LikeProvider.js";
 import {getFeedPosts} from "./PostProvider.js";
+
+const ggContainer = document.querySelector(".giffygram");
+ggContainer.addEventListener("click", (e) => {
+  if (e.target.id.startsWith("post__favorite")) {
+    let [, postId] = e.target.id.split("--");
+    postId = parseInt(postId);
+    debugger;
+    const result = toggleLike(postId);
+    if (typeof result === "string") {
+      window.alert(result);
+    } else {
+      result.then(ggContainer.dispatchEvent(new CustomEvent("stateChanged")));
+    }
+  }
+});
 
 export const postFeed = () => {
   const posts = getFeedPosts();
+  const currentUserId = parseInt(localStorage.getItem("gg_user"));
+  const filledStar = "/images/favorite-star-yellow.svg";
+  const blankStar = "/images/favorite-star-blank.svg";
   //    MAP METHOD AND JOIN METHOD
-  let htmlString = ``;
+  let htmlString = `<article class="post__feed">`;
 
   const postsHTML = posts.map((p) => {
     const postDate = new Date(p.timestamp);
+    const isLiked = p.likes.find((l) => l.userId === currentUserId);
 
-    return `
-        <div class="user__title">${p.title}</div>
-        <img src= "${p.imageURL}" style="width: 100%"/>
-        <div class="user__description">${p.description}</div>
-        <div class="user__post">Posted by ${
+    // prettier-ignore
+    return /*html*/ `<div class="post">
+        <div class="post__description">${p.title}</div>
+        <img class="post__image" src= "${p.imageURL}" style="width: 100%"/>
+        <div class="post__description">${p.description}</div>
+        <div class="user__tagline">Posted by ${
           p.userName
         } at ${postDate.toLocaleString()}.</div>
-        `;
+        <div class="post__actions"> 
+          <div>
+            <img id="post__favorite--${p.id}" class="actionIcon" src="${
+              isLiked ? filledStar : blankStar
+            }"/>
+          </div>
+        </div>
+    </div>`;
   });
 
   const listOfPosts = postsHTML.join("");
   htmlString += listOfPosts;
-  return htmlString;
+  return (htmlString += `</article>`);
 };
