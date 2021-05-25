d5a7fc5cf66a525651862c9b9711c16feba4c14c
Val Currie <contact@valcuri.email>
2021-05-25 11:05:00 -0500

    formatting

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,19 +1,13 @@
-import { setDisplayDM } from "./data/dataAccess.js";
+import {setDisplayDM} from "./data/dataAccess.js";
 import {Inbox} from "./message/inbox.js";
 import {Feed} from "./Post/Feed.js";
 
 export const GiffyGram = () => {
   // Show main main UI
   const showInbox = false;
-  setDisplayDM(false)
+  setDisplayDM(false);
   return `
     <h1>Giffygram</h1>
-    
-    
-    
     ${showInbox ? Inbox() : Feed()}
-
-  
-   
     `;
 };
