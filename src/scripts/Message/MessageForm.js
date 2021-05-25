c7e3e80f9a14d7d7578f1bd6dbd17ba7af53c068
Val Currie <contact@valcuri.email>
2021-05-25 14:18:18 -0500

    fixed eventlistener

--- src/scripts/Message/MessageForm.js
+++ src/scripts/Message/MessageForm.js
@@ -38,7 +38,7 @@ mainContainer.addEventListener("click", (clickEvent) => {
   if (clickEvent.target.id === "dm__save") {
     const userSender = parseInt(localStorage.getItem("gg_user"));
     const sendToUser = document.querySelector("#dmUser").value;
-    const sendMessage = document.querySelector("input[name='dmText']").value;
+    const sendMessage = document.querySelector("#dmText").value;
 
     const result = newMessage(userSender, sendToUser, sendMessage);
     typeof result === "string"
