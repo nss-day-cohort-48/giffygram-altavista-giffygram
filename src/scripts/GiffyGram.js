b7f5d8590481cc7b6c008dfc263f5714d4a4db23
keyanoush <keyanoush@Keyanoush.localdomain>
2021-05-26 09:21:41 -0500

    removed dead lines maked code look nice

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,14 +1,9 @@
 import {NavBar} from "./nav/NavBar.js";
-//import {setDisplayDM, setDisplayMessage} from "./data/dataAccess.js";
 import {Inbox} from "./Message/inbox.js";
 import {Feed} from "./Post/Feed.js";
 import {getFeedState} from "./data/dataAccess.js";
 
-//setDisplayDM(true); // FOR TESTING
-
 export const GiffyGram = () => {
-  // Show main main UI
-  //const showInbox = false; // FOR TESTING
   return `
   <nav>${NavBar()}</nav>
     ${getFeedState().displayMessages ? Inbox() : Feed()}
